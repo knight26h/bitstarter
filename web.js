a30b51e82c38338c29a5886ce976dc823cd0d2b5
knight26h <knight26h@gmail.com>
2013-07-09 03:54:28 +0000

    include public folder

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 var infile = "index.html";
 var fd = fs.readFileSync(infile,'utf8');
 
 //console.log(fd);
 
 var app = express.createServer(express.logger());
+app.use(express.static(path.join( __dirname,'public')));
 
 app.get('/', function(request, response) {
   response.send( fd.toString() );
